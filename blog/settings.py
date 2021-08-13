fc5089c29361d1c7055b888286e60c3845a756d6
Mitsushidu <fireminergoodplay@gmail.com>
2021-08-14 00:38:14 +0300

    'postgresql'

--- blog/settings.py
+++ blog/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'help228',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
